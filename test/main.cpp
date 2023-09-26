ca639221115148ea5d5a802ce99e5a266b0ffe2c
Arã Araújo <araaraujo.07@gmail.com>
2023-09-25 21:05:58 -0600

    adding changes

--- test/main.cpp
+++ test/main.cpp
@@ -7,9 +7,12 @@
 
 #include <iostream>
 
+using namespace std;
+
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    cout << "Hello, World!\n";
+    cout << "testing" << endl;
     return 0;
 }
 
